b0efbc19497124473624b64402eda6df620e38f6
Joshua-mm <joshuamoscosadev@gmail.com>
2021-08-19 15:49:41 -0500

    Cors¿

--- server/server.js
+++ server/server.js
@@ -32,17 +32,18 @@ app.use(require('./routes/todo'));
 app.use(require('./routes/nota'));
 
 // Cors
-/*const cors = require('cors');
-app.use(cors());*/
+const cors = require('cors');
+let whiteList = ['http://localhost:3000']
+app.use(cors( { origin: whiteList } ));
 
 // Configurar cabeceras y cors
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-});
+});*/
 
 
 // Run server
