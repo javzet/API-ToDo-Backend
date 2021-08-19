c47211282d52143b456219bc345b4dcce5f5e134
Joshua-mm <joshuamoscosadev@gmail.com>
2021-08-19 15:14:41 -0500

    Cors Inseguros

--- server/server.js
+++ server/server.js
@@ -32,8 +32,18 @@ app.use(require('./routes/todo'));
 app.use(require('./routes/nota'));
 
 // Cors
-const cors = require('cors');
-app.use(cors());
+/*const cors = require('cors');
+app.use(cors());*/
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 // Run server
 app.listen( process.env.PORT, (err) => {
